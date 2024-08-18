38419ee08ddc60db36262f81668de16f55370d07
Ryan G <98589683+ryanginn@users.noreply.github.com>
2024-08-18 04:02:31 +0100

    Update run.py
    some changes....

--- run.py
+++ run.py
@@ -1,13 +1,8 @@
 import requests
 from datetime import datetime
 import time
-
-# URL of the Icecast server stream
-url = "https://[ip]/status-json.xsl?mount=/radio2.mp3"
-
+url = "https://[ip]/status-json.xsl?mount=/[mount.mp3]"
 last_content = None
-
-# Maximum number of retries
 max_retries = 30000
 
 def fetch_and_process_metadata():
@@ -17,17 +12,27 @@ def fetch_and_process_metadata():
     while retries < max_retries:
         try:
             response = requests.get(url)
-            response.raise_for_status() 
+            response.raise_for_status()
             data = response.json()
+
             title = data.get('icestats', {}).get('source', {}).get('title', '')
+            
+            if not isinstance(title, str):
+                title = ""
+                
             prefix = "Now playing: "
+            
             if " - " in title:
                 artist, song_title = title.split(" - ", 1)
                 current_content = f"Title: {song_title} - Artist: {artist}"
-                artist_start_marker = len(prefix)  
-                artist_length_marker = len(artist) - 1 
-                song_start_marker = artist_start_marker + len(artist) + 3 
-                song_length_marker = len(song_title) - 1  
+
+                artist_start_marker = len(prefix) 
+                artist_length_marker = len(artist) - 1  
+
+                song_start_marker = artist_start_marker + len(artist) + 3  # Start after "Artist - "
+                song_length_marker = len(song_title) - 1 
+
+                # Generate the DLS Plus text output
                 dls_plus_output = f"""##### parameters {{ #####
 DL_PLUS=1
 DL_PLUS_ITEM_TOGGLE=0
@@ -41,25 +46,31 @@ DL_PLUS_TAG=1 {song_start_marker} {song_length_marker}
                 
                 with open('/home/ryan/dab/mot/P02/INFO.dls', 'w') as file:
                     file.write(dls_plus_output)
+
             else:
                 current_content = title
                 with open('/home/ryan/dab/mot/P02/INFO.dls', 'w') as file:
                     file.write(title)
+
             if current_content != last_content:
                 last_content = current_content
+
                 current_time = datetime.now().strftime("%H:%M:%S")
+
                 print(f"[{current_time}] - Content updated: {current_content}")
             else:
                 print("No content update needed.")
+
             break
 
         except (requests.RequestException, ValueError) as e:
             print(f"Error: {e}. Retrying ({retries + 1}/{max_retries})...")
             retries += 1
-            time.sleep(1)
+            time.sleep(1) 
 
     if retries == max_retries:
         print("Failed to fetch data after maximum retries. Stopping the program.")
+
 while True:
     fetch_and_process_metadata()
-    time.sleep(5)
+    time.sleep(5)  # Wait for 5 seconds before the next check
